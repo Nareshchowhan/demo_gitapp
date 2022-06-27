b487318f566050855433f8548d4d7f7894044676
Naresh <nkorra481@gmail.com>
2022-06-27 17:02:13 +0530

    new commit adding directly

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ console.log("created new branch");
 
 console.log("created pull request");
 console.log("created one more time for pull request");
+console.log("adding directly from vs code one br to another  br");
